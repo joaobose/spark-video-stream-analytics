e08ecb51ef0feadfc6e82d6f347cf6e795fe6228
joaobose <joaobose@gmail.com>
2024-05-15 19:12:08 -0400

    Montion detection demo: Showing abs difference map

--- src/vision.py
+++ src/vision.py
@@ -4,7 +4,7 @@ import cv2 as cv
 def check_motion_v1(prev_frame, current_frame):
     """
     Check if there is motion between two frames.
-    Returns if there is motion and the frame with the motion highlighted.
+    Returns if there is motion, the frame with the motion highlighted and the diff map.
 
     The method used in this funciton is exactly the same as the Java
     implementation of the original project. 
@@ -24,10 +24,10 @@ def check_motion_v1(prev_frame, current_frame):
     current_gray = cv.GaussianBlur(current_gray, (3, 3), 0)
 
     # Compute the absolute difference between the two frames
-    frame_diff = cv.absdiff(prev_gray, current_gray)
+    abs_diff = cv.absdiff(prev_gray, current_gray)
 
     # Apply a threshold to the difference
-    _, frame_diff = cv.threshold(frame_diff, 20, 255, cv.THRESH_BINARY)
+    _, frame_diff = cv.threshold(abs_diff, 20, 255, cv.THRESH_BINARY)
 
     # Get the contours of the difference
     contours, _ = cv.findContours(
@@ -46,6 +46,6 @@ def check_motion_v1(prev_frame, current_frame):
             cv.rectangle(current_frame_copy, (x, y),
                          (x + w, y + h), (0, 255, 0), 2)
 
-        return True, current_frame_copy
+        return True, current_frame_copy, abs_diff
 
-    return False, current_frame_copy
+    return False, current_frame_copy, abs_diff
